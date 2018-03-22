26691c2ffc7bb199bd73587cb87093ffce8a4d3d
Wouter Lindenhof <wouterlindenhof@gmail.com>
2018-03-22 20:42:44 +0100

    Add create-account validation

--- api/src/graphql/account/mutations/create-account.mutation.ts
+++ api/src/graphql/account/mutations/create-account.mutation.ts
@@ -6,7 +6,29 @@ import * as bcrypt from "bcryptjs";
 import { SecurityConfig } from "../../../config";
 
 
+function throwIfInvalid(input: CreateAccountInput): void {
+    const errors: string[] = [];
+    const emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (!emailRegex.test(input.email)) {
+        errors.push("Email is invalid");
+    }
+
+    if (input.password.length === 0) {
+        errors.push("Empty password is not allowed");
+    }
+
+    if (input.password.trim().length !== input.password.length) {
+        errors.push("Password can not contain whitespace at start or end");
+    }
+
+    if (errors.length !== 0) {
+        throw new Error("Input is invalid: " + errors.join(", "));
+    }
+}
+
 export async function createAccount(obj, args: { input: CreateAccountInput }, context: GraphContext, info): Promise<Account> {
+    throwIfInvalid(args.input);
     const entityManager = context.entityManager;
     const account = entityManager.create(AccountEntity);
     account.email = args.input.email;
