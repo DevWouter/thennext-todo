c20082ca518c857b7f1b937d9e863e5d460265ad
Wouter Lindenhof <wouterlindenhof@gmail.com>
2018-04-07 20:37:18 +0200

    Add scripts to make database migration simple

---
+++ api/scripts/run-migrations.sh
@@ -0,0 +1,4 @@
+
+# The following script runs the migrations on the docker container
+
+docker-compose exec api npm run orm:run-migrations
---
+++ api/scripts/show-changes.sh
@@ -0,0 +1,4 @@
+
+# The following script shows the changes for the migration
+
+docker-compose exec api npm run orm:changes
