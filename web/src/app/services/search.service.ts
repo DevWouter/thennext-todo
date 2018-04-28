4c0303ab051342ef26eb11eaf7d708efd707506b
Wouter Lindenhof <wouterlindenhof@gmail.com>
2018-04-28 18:41:24 +0200

    Make search case-insensitive

--- web/src/app/services/search.service.ts
+++ web/src/app/services/search.service.ts
@@ -10,11 +10,11 @@ export class SearchService {
     if (query === undefined || query === null || query.trim().length === 0) {
       return true;
     }
-    const queryTerms = query.split(" ").filter(x => x.length > 0);
+    const queryTerms = query.toLowerCase().split(" ").filter(x => x.length > 0);
 
     // Needs to contain at least one term.
     return queryTerms.some(term => {
-      return tv.task.title.includes(term);
+      return tv.task.title.toLowerCase().includes(term);
     });
   }
 }
