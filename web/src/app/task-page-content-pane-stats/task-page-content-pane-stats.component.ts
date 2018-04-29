76e89e9285112100630846f1236add757d2fcfd8
Wouter Lindenhof <wouterlindenhof@gmail.com>
2018-04-29 16:04:45 +0200

    Add show completed in task-pane-stat

--- web/src/app/task-page-content-pane-stats/task-page-content-pane-stats.component.ts
+++ web/src/app/task-page-content-pane-stats/task-page-content-pane-stats.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { ContextService } from "../services/context.service";
-import { Task } from "../services/models/task.dto";
+import { Task, TaskStatus } from "../services/models/task.dto";
 import { TaskService } from "../services/task.service";
 import { TaskView } from "../services/models/task-view";
 import { NavigationService } from "../services/navigation.service";
@@ -13,14 +13,18 @@ import { NavigationService } from "../services/navigation.service";
 export class TaskPageContentPaneStatsComponent implements OnInit {
 
   private _taskView: TaskView;
+  private _task: Task;
   public createdOn: Date;
   public updatedOn: Date;
   public completedOn: Date;
   public sleepUntil: Date;
-  public score: number;
 
   get showDelay(): boolean {
     return !!this.sleepUntil;
+  }
+
+  get showCompleted(): boolean {
+    return this._task && this._task.status === TaskStatus.done;
   }
 
   constructor(
@@ -36,16 +40,16 @@ export class TaskPageContentPaneStatsComponent implements OnInit {
   private setTask(taskView: TaskView) {
     this._taskView = taskView;
     if (taskView) {
+      this._task = taskView.task;
       this.createdOn = taskView.task.createdOn;
       this.updatedOn = taskView.task.updatedOn;
       this.completedOn = taskView.task.completedOn;
       this.sleepUntil = taskView.task.sleepUntil;
-      this.score = taskView.score;
     } else {
+      this._task = undefined;
       this.createdOn = undefined;
       this.updatedOn = undefined;
       this.completedOn = undefined;
-      this.score = undefined;
     }
   }
 
