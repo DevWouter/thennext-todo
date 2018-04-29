ae1b8c524c71cf7ab7408e410701a3ba226363a1
Wouter Lindenhof <wouterlindenhof@gmail.com>
2018-04-29 19:50:36 +0200

    Add debounce to checklist-item

--- web/src/app/task-page-content-pane-checklist-item/task-page-content-pane-checklist-item.component.ts
+++ web/src/app/task-page-content-pane-checklist-item/task-page-content-pane-checklist-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { ChecklistItem } from "../services/models/checklist-item.dto";
 import { ChecklistItemService } from "../services/checklist-item.service";
+import { BehaviorSubject } from "rxjs/BehaviorSubject";
 
 @Component({
   selector: "app-task-page-content-pane-checklist-item",
@@ -9,38 +10,58 @@ import { ChecklistItemService } from "../services/checklist-item.service";
 })
 export class TaskPageContentPaneChecklistItemComponent implements OnInit {
 
-  public title: string;
-  public checked = false;
+  private _titleSubject = new BehaviorSubject<string>(undefined);
+  public get title(): string { return this._item.title; }
+  public set title(v: string) { this._item.title = v; this._titleSubject.next(v); }
 
+  private _checkedSubject = new BehaviorSubject<boolean>(undefined);
+  public get checked(): boolean { return this._item.checked; }
+  public set checked(v: boolean) { this._item.checked = v; this._checkedSubject.next(v); }
+
+  private _item: ChecklistItem = undefined;
   @Input()
-  public item: ChecklistItem;
+  public set item(v: ChecklistItem) {
+    // Mark as complete.
+    this._titleSubject.complete();
+    this._checkedSubject.complete();
+
+    this._item = v;
+
+    this._titleSubject = new BehaviorSubject<string>(v.title);
+    this._checkedSubject = new BehaviorSubject<boolean>(v.checked);
+
+    // Update is not called when the new title is empty
+    this._titleSubject
+      .filter(x => x !== undefined && x.length !== 0)
+      .combineLatest(this._checkedSubject, (title, checked) => {
+        v.title = title;
+        v.checked = checked;
+        return v;
+      })
+      .debounceTime(350)
+      .subscribe(x => {
+        this.checklistItemService.update(x);
+      });
+  }
+
+  public get item(): ChecklistItem {
+    return this._item;
+  }
 
   constructor(
     private readonly checklistItemService: ChecklistItemService,
   ) { }
 
   ngOnInit() {
-    this.title = this.item.title;
-    this.checked = this.item.checked;
   }
 
   updateTitle() {
-    if (this.item.title === this.title) {
-      // No changes.
-      return;
-    }
-
-    this.item.title = this.title;
-    if (this.item.title.length === 0) {
-      this.checklistItemService.delete(this.item);
-    } else {
-      this.checklistItemService.update(this.item);
+    if (this._item.title.length === 0) {
+      this.checklistItemService.delete(this._item);
     }
   }
 
   toggle() {
     this.checked = !this.checked;
-    this.item.checked = this.checked;
-    this.checklistItemService.update(this.item);
   }
 }
