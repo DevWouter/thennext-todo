97ae7bb7d600be65452e0420b19bd525dadc7f51
Wouter Lindenhof <wouterlindenhof@gmail.com>
2018-04-29 17:24:22 +0200

    Improve performance of description

--- web/src/app/task-page-content-pane-description/task-page-content-pane-description.component.ts
+++ web/src/app/task-page-content-pane-description/task-page-content-pane-description.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
 import { ContextService } from "../services/context.service";
 import { TaskService } from "../services/task.service";
 import { Task } from "../services/models/task.dto";
 import { TaskView } from "../services/models/task-view";
+import { Subject } from "rxjs/Subject";
 
 @Component({
   selector: "app-task-page-content-pane-description",
@@ -10,36 +11,32 @@ import { TaskView } from "../services/models/task-view";
   styleUrls: ["./task-page-content-pane-description.component.scss"]
 })
 export class TaskPageContentPaneDescriptionComponent implements OnInit {
-  private taskView: TaskView;
   private _value: string;
+  private _nextValue = new Subject<string>();
+
   public get value(): string { return this._value; }
-  public set value(v: string) { this._value = v; this.update(); }
+  public set value(v: string) { this._value = v; this._nextValue.next(v); }
+
+  @Input()
+  public set task(task: Task) {
+    this._value = task.description;
+    this._nextValue.complete();
+
+    this._nextValue = new Subject<string>();
+    this._nextValue.subscribe(v => task.description = v);
+    this._nextValue
+      .debounceTime(350)
+      .subscribe(v => {
+        task.description = v;
+        this.taskService.update(task);
+      });
+  }
 
   constructor(
-    private contextService: ContextService,
     private taskService: TaskService,
   ) {
   }
 
   ngOnInit() {
-    this.setup();
   }
-
-
-  private setup() {
-    this.contextService.activeTaskView.subscribe((x) => {
-      this.taskView = x;
-      if (this.taskView) {
-        this._value = this.taskView.task.description;
-      }
-    });
-  }
-
-  private update() {
-    if (this.taskView) {
-      this.taskView.task.description = this.value;
-      this.taskService.update(this.taskView.task);
-    }
-  }
-
 }
