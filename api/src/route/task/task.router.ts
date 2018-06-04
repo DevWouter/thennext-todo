22cade6fc5c3598a847dd0c4a0468c548e5c18b5
Wouter Lindenhof <wouterlindenhof@gmail.com>
2018-06-04 23:11:21 +0200

    Convert task-func to task-controller

--- api/src/route/task/task.router.ts
+++ api/src/route/task/task.router.ts
@@ -1,16 +1,16 @@
 import * as express from "express";
+import container from "../../inversify.config";
 import { asyncMiddleware } from "../../helpers/async-helper";
 
-import { TaskIndex } from "./task-index";
-import { TaskCreate } from "./task-create";
-import { TaskUpdate } from "./task-update";
-import { TaskDelete } from "./task-delete";
+import { TaskController } from "./task.controller";
+
+const controller = container.resolve(TaskController);
 
 const router = express.Router();
 
-router.get("/index", asyncMiddleware(TaskIndex));
-router.post("/create", asyncMiddleware(TaskCreate));
-router.patch("/:uuid", asyncMiddleware(TaskUpdate));
-router.delete("/:uuid", asyncMiddleware(TaskDelete));
+router.get("/index", asyncMiddleware(controller.index.bind(controller)));
+router.post("/create", asyncMiddleware(controller.create.bind(controller)));
+router.patch("/:uuid", asyncMiddleware(controller.update.bind(controller)));
+router.delete("/:uuid", asyncMiddleware(controller.delete.bind(controller)));
 
 export { router as taskRouter };
