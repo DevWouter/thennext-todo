afc8cc9f6dc23dbf4b9f00b36245a3b3364378e7
Wouter Lindenhof <wouterlindenhof@gmail.com>
2018-06-18 21:33:49 +0200

    Add title component for settings-tab-pages

--- web/src/app/settings-page/settings-page.module.ts
+++ web/src/app/settings-page/settings-page.module.ts
@@ -9,6 +9,7 @@ import { SettingsPageScoreshiftsComponent } from "./settings-page-scoreshifts/se
 import { SettingsPageTabScoringComponent } from "./settings-page-tab-scoring/settings-page-tab-scoring.component";
 import { SettingsPageTabTasklistsComponent } from "./settings-page-tab-tasklists/settings-page-tab-tasklists.component";
 import { SettingsPageTasklistsComponent } from "./settings-page-tasklists/settings-page-tasklists.component";
+import { SettingsContentTiltleComponent } from "./settings-content-title/settings-content-title.component";
 import { AcceptShareTokenComponent } from "./accept-share-token/accept-share-token.component";
 
 import { TabTitlesComponent } from "./tab-titles/tab-titles.component";
@@ -34,6 +35,7 @@ import { DialogsModule } from "../dialogs/dialogs.module";
     SettingsPageScoreshiftsComponent,
     SettingsTasklistComponent,
     AcceptShareTokenComponent,
+    SettingsContentTiltleComponent,
   ],
   exports: [TabTitlesComponent]
 })
