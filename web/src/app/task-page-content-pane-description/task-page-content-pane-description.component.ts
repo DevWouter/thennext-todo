a977616dbcbc395ff987650d41c31e99622baae7
Wouter Lindenhof <wouterlindenhof@gmail.com>
2018-07-16 19:07:26 +0200

    Remove derefence of description in pane
    Using the task object directly now

--- web/src/app/task-page-content-pane-description/task-page-content-pane-description.component.ts
+++ web/src/app/task-page-content-pane-description/task-page-content-pane-description.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit, Input } from "@angular/core";
-import { ContextService } from "../services/context.service";
 import { TaskService } from "../services/task.service";
 import { Task } from "../services/models/task.dto";
-import { Subject } from "rxjs";
-import { debounceTime } from "rxjs/operators";
 
 @Component({
   selector: "app-task-page-content-pane-description",
@@ -11,25 +8,23 @@ import { debounceTime } from "rxjs/operators";
   styleUrls: ["./task-page-content-pane-description.component.scss"]
 })
 export class TaskPageContentPaneDescriptionComponent implements OnInit {
-  private _value: string;
-  private _nextValue = new Subject<string>();
+  private _task: Task;
 
-  public get value(): string { return this._value; }
-  public set value(v: string) { this._value = v; this._nextValue.next(v); }
+  public get value(): string { return this._task && this._task.description; }
+  public set value(v: string) {
+    if (this._task) {
+      if (this._task.description === v) {
+        return; // No change
+      }
+
+      this._task.description = v;
+      this.taskService.update(this._task);
+    }
+  }
 
   @Input()
   public set task(task: Task) {
-    this._value = task.description;
-    this._nextValue.complete();
-
-    this._nextValue = new Subject<string>();
-    this._nextValue.subscribe(v => task.description = v);
-    this._nextValue
-      .pipe(debounceTime(350))
-      .subscribe(v => {
-        task.description = v;
-        this.taskService.update(task);
-      });
+    this._task = task;
   }
 
   constructor(
