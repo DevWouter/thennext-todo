87ab04d3b59f98b4e162b2df219f03db72d2cd66
Wouter Lindenhof <wouterlindenhof@gmail.com>
2018-08-01 21:19:59 +0200

    Add MailService

--- api/src/services/mail-service.ts
+++ api/src/services/mail-service.ts
@@ -1,5 +1,16 @@
 import { injectable } from "inversify";
 
+import SendGrid = require('@sendgrid/mail');
+
+interface TemplateMap {
+    "PlainMessage": { subject: string, content: string };
+}
+
+const templateGuids = {
+    "PlainMessage": "d-cade11b620d24d5598123b6f4414c4da",
+};
+
+
 @injectable()
 export class MailService {
     constructor(
@@ -8,5 +19,35 @@ export class MailService {
     }
 
     private setup(): void {
+        if (process.env.SENDGRID_API_KEY === undefined ||
+            process.env.SENDGRID_API_KEY === null ||
+            process.env.SENDGRID_API_KEY === ""
+        ) {
+            throw new Error("Please use `export SENDGRID_API_KEY='YOUR_API_KEY'` to set the key");
+        }
+        SendGrid.setApiKey(process.env.SENDGRID_API_KEY);
+        SendGrid.setSubstitutionWrappers('{{', '}}'); // Configure the substitution tag wrappers globally
+    }
+
+    async sendMessage<K extends keyof TemplateMap>(
+        template: K,
+        to: string,
+        templateData: TemplateMap[K]
+    ): Promise<boolean> {
+        const msg = {
+            to: to,
+            from: 'messages@thennext.com',
+            templateId: templateGuids[template],
+            substitutionWrappers: ['{{', '}}'],
+            dynamicTemplateData: templateData,
+        };
+        try {
+            await SendGrid.send(msg);
+            return true;
+        } catch (error) {
+            console.error("Unable to send mail", error);
+            return false;
+        }
+
     }
 }
