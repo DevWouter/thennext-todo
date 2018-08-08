187d378121292ef23551c999494e4b3fa74d5bb9
Wouter Lindenhof <wouterlindenhof@gmail.com>
2018-08-08 20:14:09 +0200

    Add support for queries that are multiple lines

--- api/src/repositories/database.ts
+++ api/src/repositories/database.ts
@@ -1,4 +1,5 @@
 import { Connection, FieldInfo } from "mysql";
+import { isArray } from "util";
 
 
 export class Database {
@@ -8,9 +9,13 @@ export class Database {
     ) {
     }
 
-    execute(query: string, values?: any): Promise<{ results: any, fields: FieldInfo[] }> {
+    execute(query: string | string[], values?: any): Promise<{ results: any, fields: FieldInfo[] }> {
         return new Promise((resolve, reject) => {
-            this.connection.query(query, values, (error, results, fields) => {
+            if (isArray(query)) {
+                query = query.join("\n");
+            }
+
+            this.connection.query(query as string, values, (error, results, fields) => {
                 if (error) {
                     console.error(`ERROR WHILE EXECUTING QUERY: ${error}`);
                     console.error("-------- START OF QUERY --------");
