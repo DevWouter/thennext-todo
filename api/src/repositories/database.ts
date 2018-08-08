d27c3050c80ae6bbfdbce64ecacee8b826ab2353
Wouter Lindenhof <wouterlindenhof@gmail.com>
2018-08-08 19:54:35 +0200

    On error write the query to the console

--- api/src/repositories/database.ts
+++ api/src/repositories/database.ts
@@ -12,6 +12,11 @@ export class Database {
         return new Promise((resolve, reject) => {
             this.connection.query(query, values, (error, results, fields) => {
                 if (error) {
+                    console.error(`ERROR WHILE EXECUTING QUERY: ${error}`);
+                    console.error("-------- START OF QUERY --------");
+                    console.error(query);
+                    console.error("-------- END OF QUERY --------");
+
                     reject(error);
                 }
 
