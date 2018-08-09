1c7c0c86465daee06dc2ac569638271b7c9567e9
Wouter Lindenhof <wouterlindenhof@gmail.com>
2018-08-09 16:00:57 +0200

    Database ignore properties that are undefined

--- api/src/repositories/database.ts
+++ api/src/repositories/database.ts
@@ -1,8 +1,8 @@
 import { Connection, FieldInfo } from "mysql";
 import { isArray } from "util";
+import { isDate } from "moment";
 
 export const uuidv4: () => string = require('uuid/v4');
-
 export class Database {
 
     constructor(
@@ -11,14 +11,14 @@ export class Database {
     }
 
     async insert<T>(tablename: string, row: Partial<T>): Promise<number> {
-        const propertyNames = Object.getOwnPropertyNames(row);
+        const propertyNames = Object.getOwnPropertyNames(row).filter(x => row[x] !== undefined);
         const query = `INSERT INTO \`${tablename}\` \n` +
             `  (${propertyNames.reduce((prev, cur) => (prev ? prev + ", " : "") + "??", undefined)}) \n` +
             `  VALUES (${propertyNames.reduce((prev, cur) => (prev ? prev + ", " : "") + "?", undefined)})`;
 
         const { results, fields } = await this.execute(query, [
             ...propertyNames,
-            ...Object.values(row),
+            ...propertyNames.map(x => row[x]),
         ]);
 
         return results.insertId;
@@ -28,8 +28,8 @@ export class Database {
         row: Partial<T>,
         filter: Partial<T>,
         limit: number | "all"): Promise<void> {
-        const rowProperties = Object.getOwnPropertyNames(row);
-        const filterProperties = Object.getOwnPropertyNames(filter);
+        const rowProperties = Object.getOwnPropertyNames(row).filter(x => row[x] !== undefined);
+        const filterProperties = Object.getOwnPropertyNames(filter).filter(x => filter[x] !== undefined);
         const query = `UPDATE \`${tablename}\` \n` +
             `\n  SET ${rowProperties.reduce((prev, cur) => (prev ? prev + ", " : "") + "?? = ?", undefined)} \n` +
             `\n  WHERE 1=1 AND (${filterProperties.reduce((prev, cur) => (prev ? prev + " AND " : "") + "?? = ?", undefined)})` +
@@ -43,7 +43,7 @@ export class Database {
 
 
     async delete<T>(tablename: string, filter: Partial<T>, limit: number | "all"): Promise<number> {
-        const propertyNames = Object.getOwnPropertyNames(filter);
+        const propertyNames = Object.getOwnPropertyNames(filter).filter(x => filter[x] !== undefined);
 
         if (propertyNames.length === 0) {
             // In theory we could allow unfiltered rows to be allowed, but that would result in `DELETE FROM table`
@@ -62,18 +62,24 @@ export class Database {
         return results.affectedRows;
     }
 
-    execute(query: string | string[], values?: any): Promise<{ results: any, fields: FieldInfo[] }> {
+    execute(query: string | string[], values?: any[]): Promise<{ results: any, fields: FieldInfo[] }> {
         return new Promise((resolve, reject) => {
             if (isArray(query)) {
                 query = query.join("\n");
             }
+
+            /*
+            console.log("-------- START OF QUERY --------");
+            console.log(this.connection.format(query, values));
+            console.log("-------- END OF QUERY --------");
+            // */
 
             this.connection.query(query as string, values, (error, results, fields) => {
                 if (error) {
                     console.error(`ERROR WHILE EXECUTING QUERY: ${error}`);
                     console.error("-------- START OF QUERY --------");
                     console.error(query);
-                    console.error(values);
+                    // console.error(values);
                     console.error("-------- END OF QUERY --------");
 
                     reject(error);
