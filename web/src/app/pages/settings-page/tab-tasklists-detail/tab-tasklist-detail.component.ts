619e8ebf503a889ace20315f3357336af6549128
Wouter Lindenhof <wouterlindenhof@gmail.com>
2018-08-13 21:02:38 +0200

    Removed the ability to share tasklists

--- web/src/app/pages/settings-page/tab-tasklists-detail/tab-tasklist-detail.component.ts
+++ web/src/app/pages/settings-page/tab-tasklists-detail/tab-tasklist-detail.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { combineLatest } from 'rxjs';
-
-import { TaskListService, TaskListRightService, TaskListShareTokenService } from '../../../services';
-import { TaskList, TaskListRight, TaskListShareToken } from '../../../models';
 import { map } from 'rxjs/operators';
+
+import { TaskListService } from '../../../services';
+import { TaskList } from '../../../models';
 
 @Component({
   selector: 'settings-tab-tasklist-detail',
@@ -13,15 +13,10 @@ import { map } from 'rxjs/operators';
 })
 export class SettingsTasklistDetailComponent implements OnInit {
   taskList: TaskList;
-  rights: TaskListRight[] = undefined;
-  tokens: TaskListShareToken[] = undefined;
-  newToken = "";
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private tasklistService: TaskListService,
-    private tasklistRightService: TaskListRightService,
-    private tasklistShareTokenService: TaskListShareTokenService,
   ) { }
 
   ngOnInit() {
@@ -30,38 +25,5 @@ export class SettingsTasklistDetailComponent implements OnInit {
       .pipe(map(([entries, uuid]) => entries.find(e => e.uuid === uuid)));
 
     $currentTaskList.subscribe(t => this.taskList = t);
-
-    combineLatest($currentTaskList, this.tasklistRightService.entries)
-      .pipe(map(([list, rights]) => rights.filter(r => r.taskListUuid === list.uuid)))
-      .subscribe(t => this.rights = t);
-
-    combineLatest($currentTaskList, this.tasklistShareTokenService.entries)
-      .pipe(map(([list, tokens]) => tokens.filter(r => r.taskListUuid === list.uuid)))
-      .subscribe(x => this.tokens = x)
-      ;
-  }
-
-  delete(right: TaskListRight) {
-    this.tasklistRightService.delete(right);
-  }
-  deleteToken(token: TaskListShareToken) {
-    this.tasklistShareTokenService.delete(token);
-  }
-
-  addShareToken() {
-    this.newToken = this.newToken.trim();
-    // Don't allow empty tokens.
-    if (this.newToken.length === 0) {
-      return;
-    }
-
-    const newToken = <TaskListShareToken>{
-      taskListUuid: this.taskList.uuid,
-      token: this.newToken,
-    };
-
-    this.newToken = "";
-
-    this.tasklistShareTokenService.add(newToken);
   }
 }
