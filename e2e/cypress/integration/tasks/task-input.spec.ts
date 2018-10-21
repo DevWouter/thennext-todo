461e2d018eb111e3a3b94b803e0e4bee018ad84c
Wouter Lindenhof <wouterlindenhof@gmail.com>
2018-10-21 10:40:01 +0200

    Rewrote task-input.spec to follow gherkin style

--- e2e/cypress/integration/tasks/task-input.spec.ts
+++ e2e/cypress/integration/tasks/task-input.spec.ts
@@ -1,36 +1,79 @@
 describe("Tasks - Task Input", () => {
-    before(() => {
-        // Perform login
-        cy.ensureTestAccount();
-        cy.loginTestAccount();
-        cy.visit("/tasks");
+
+    describe("Default", () => {
+        it("Given a new account", () => {
+            // Perform login
+            cy.ensureTestAccount();
+            cy.loginTestAccount();
+            cy.visit("/tasks");
+        });
+
+        it("Should not have any tasks", () => {
+            cy.get('[data-cy=no-tasks-found]').should("be.visible");
+        });
     });
 
-    it("Should show by default 'no tasks could be found'", () => {
-        cy.get('[data-cy=no-tasks-found]').should("be.visible");
+    describe("Create first task", () => {
+        it("When I type 'Hello' and press enter in Input", () => {
+            cy.get("[data-cy=task-input]").type("Hello{enter}");
+        });
+
+        it("Then the input input should be empty", () => {
+            cy.get("[data-cy=task-input]").should("not.have.text");
+        });
+
+        it("Then the task-list should contain the first task", () => {
+            cy.get("app-tasklist-item").contains("Hello").should("exist");
+        });
     });
 
-    it("Should create a task (first)", () => {
-        cy.get("[data-cy=task-input]").type("Hello{enter}");
-        cy.get("[data-cy=task-input]").should("not.have.text");
-        cy.contains("Hello").should("be.visible");
+    describe("Entering a second task", () => {
+        it("When I type 'World' in the input", () => {
+            cy.get("[data-cy=task-input]").type("World");
+        });
+        it("Then the 'no-tasks could be found' should be visible", () => {
+            cy.get('[data-cy=no-tasks-found]').should("be.visible");
+        });
+        it("Then the tasklist should be empty", () => {
+            cy.get("app-tasklist-item").should("have.length", 0);
+        });
     });
 
-    it("Should show 'no tasks could be found' when searching for another task name", () => {
-        cy.get("[data-cy=task-input]").type("World");
-        cy.get('[data-cy=no-tasks-found]').should("be.visible");
+    describe("Create the second task", () => {
+        it("When I press enter in the input", () => {
+            cy.get("[data-cy=task-input]").type("{enter}");
+        });
+
+        it("Then the input input should be empty", () => {
+            cy.get("[data-cy=task-input]").should("not.have.text");
+        });
+
+        it("Then the tasklist should contain two items: Hello and World", () => {
+            cy.get("app-tasklist-item").should("have.length", 2);
+            cy.get("app-tasklist-item").contains("Hello").should("be.visible");
+            cy.get("app-tasklist-item").contains("World").should("be.visible");
+        });
     });
 
-    it("Should create a task (second)", () => {
-        cy.get("[data-cy=task-input]").type("{enter}");
-        cy.get("[data-cy=task-input]").should("not.have.text");
-        cy.contains("World").should("be.visible");
+    describe("Filter for the second task", () => {
+        it("When I type 'World' in the input", () => {
+            cy.get("[data-cy=task-input]").type("World");
+        });
+        it("Then the tasklist should contain one item: World", () => {
+            cy.get("app-tasklist-item").should("have.length", 1);
+            cy.get("app-tasklist-item").contains("World").should("be.visible");
+        });
     });
 
-    it("Should only show the second task when filtering for the second task", () => {
-        cy.get("[data-cy=task-input]").type("World");
-        cy.get("[data-cy=task-list-container]")
-            .get("app-tasklist-item")
-            .should("have.length", 1);
-    });
+    describe("Clearing the filter shows all tasks again", () => {
+        it("When I clear the filter", () => {
+            cy.get("[data-cy=task-input]").type("{selectall}{backspace}");
+        });
+
+        it("Then the tasklist should contain two items: Hello and World", () => {
+            cy.get("app-tasklist-item").should("have.length", 2);
+            cy.get("app-tasklist-item").contains("Hello").should("be.visible");
+            cy.get("app-tasklist-item").contains("World").should("be.visible");
+        });
+    })
 })
