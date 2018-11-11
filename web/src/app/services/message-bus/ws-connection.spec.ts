80f417e85f383077496d85a855b22406f6a5a042
Wouter Lindenhof <wouterlindenhof@gmail.com>
2018-11-11 21:30:41 +0100

    Implemented WsConnection with tests

--- web/src/app/services/message-bus/ws-connection.spec.ts
+++ web/src/app/services/message-bus/ws-connection.spec.ts
@@ -1,4 +1,4 @@
-import { WsConnection } from "./ws-connection";
+import { WsConnection, WsConnectionInterface, WsConnectionControlInterface } from "./ws-connection";
 
 import {
   WebSocket as FakeSocket,
@@ -6,10 +6,12 @@ import {
 } from "mock-socket";
 
 import { WsEvent } from "../ws/events";
+import { skip, take } from "rxjs/operators";
+import { SetTokenCommand } from "../ws/commands";
 
-xdescribe('WsConnection', () => {
+describe('WsConnection', () => {
   let ORIGINAL_WEBSOCKET = undefined;
-  let connection: WsConnection;
+  let connection: WsConnectionInterface & WsConnectionControlInterface;
   let server: FakeServer;
 
   beforeAll(() => {
@@ -25,17 +27,169 @@ xdescribe('WsConnection', () => {
 
   beforeEach(() => {
     server = new FakeServer("ws://localhost");
-    connection = new WsConnection();
+    connection = new WsConnection({
+      url: "ws://localhost"
+    });
   });
 
   afterEach(() => {
     server.close();
   });
 
-  it("should report messages send by server");
-  it("should send commands to the server");
-  it("should open a connection when subscribing");
-  it("should report a disconnect");
-  it("should have a state subscription event");
-  it("should thrown an error when `send` is called when nothing is subscribed to the events");
+  it("should connect when `open()` command is called", (done) => {
+    server.on("connection", (socket) => {
+      expect().nothing();
+      done();
+    });
+
+    connection.open();
+  });
+
+  it("should throw an error when `open()` is called for a second time", () => {
+    const call = () => connection.open();
+    expect(call).not.toThrowError();
+    expect(call).toThrowError("connection is already created");
+  });
+
+  it("should disconnect when `close()` command is called", (done) => {
+    server.on("connection", (socket) => {
+      socket.addEventListener("close", () => {
+        expect().nothing();
+        done();
+      });
+    });
+
+    connection.open();
+    connection.close();
+  });
+
+  it("should throw an error when `close()` is called before the connection is opened", () => {
+    const call = () => connection.close();
+    expect(call).toThrowError("connection is closed, call `open` first");
+  });
+
+  it("should throw an error when `close()` is called a second time after it was opened", () => {
+    connection.open();
+    const call = () => connection.close();
+    call(); // First call it normally.
+    expect(call).toThrowError("connection is closed, call `open` first");
+  });
+
+  it("should emit status `closed` when no conneciton was made", (done) => {
+    connection.status().subscribe(ev => {
+      expect(ev.status).toBe("closed");
+      expect(ev.origin).toBe("client");
+      done();
+    });
+  });
+
+  it("should emit status `connecting` as status when `open()` is called", (done) => {
+    connection.status()
+      .pipe(skip(1), take(1))
+      .subscribe(x => {
+        expect(x.status).toBe("connecting");
+        done();
+      });
+
+    connection.open();
+  });
+
+  it("should emit status `connected` as status when connection is established", (done) => {
+    connection.status()
+      .pipe(skip(2), take(1))
+      .subscribe(x => {
+        expect(x.status).toBe("connected");
+        done();
+      });
+
+    connection.open();
+  });
+
+  it("should emit status `closing by client` when the client calls `close()`", (done) => {
+    server.on("connection", socket => {
+      // When connected let the client close the connection.
+      connection.close();
+    });
+
+    connection.status()
+      .pipe(skip(3), take(1))
+      .subscribe(x => {
+        expect(x.status).toBe("closing");
+        expect(x.origin).toBe("client");
+        done();
+      });
+
+    connection.open();
+  });
+
+  it("should emit status `closed by client` when the client has closed the connection", (done) => {
+    server.on("connection", socket => {
+      // When connected let the client close the connection.
+      connection.close();
+    });
+
+    connection.status()
+      .pipe(skip(4), take(1))
+      .subscribe(x => {
+        expect(x.status).toBe("closed");
+        expect(x.origin).toBe("client");
+        done();
+      });
+
+    connection.open();
+  });
+
+  it("should emit status `closed by server` when server has closed the connection", (done) => {
+    server.on("connection", socket => {
+      // When connected let the client close the connection.
+      socket.close();
+    });
+
+    connection.status()
+      .pipe(skip(3), take(1))
+      .subscribe(x => {
+        expect(x.status).toBe("closed");
+        expect(x.origin).toBe("server");
+        done();
+      });
+
+    connection.open();
+  });
+
+  it("should report messages send by the server", (done) => {
+    server.on("connection", socket => {
+      socket.send(JSON.stringify(<WsEvent<"token-accepted">>{
+        data: {},
+        echo: true,
+        refId: "answer",
+        type: "token-accepted"
+      }));
+    });
+
+    connection.open();
+    connection.events().subscribe(ev => {
+      expect(ev.type).toBe("token-accepted");
+      expect(ev.refId).toBe("answer");
+      done();
+    });
+  });
+
+  it("should send commands to the server", (done) => {
+    server.on("connection", socket => {
+      socket["on"]("message", (data) => {
+        const cmd = JSON.parse(data) as { command: string, data: SetTokenCommand };
+        expect(cmd.data.token).toBe("abcdef");
+        done();
+      });
+    });
+
+    connection.open();
+    connection.send("set-token", { token: "abcdef" });
+  });
+
+  it("should throw an error when sending a message when the connection is not open", () => {
+    const call = () => connection.send("set-token", { token: "abcdef" });
+
+    expect(call).toThrowError("connection is closed, call `open` first");
+  });
 });
--- web/src/app/services/message-bus/ws-connection.ts
+++ web/src/app/services/message-bus/ws-connection.ts
@@ -1,23 +1,95 @@
-import { Observable } from "rxjs";
+import { Observable, BehaviorSubject, Subject } from "rxjs";
 import { WsEventBasic } from "../ws/events";
 import { WsCommandMap } from "../ws/commands";
-import { WebSocketSubject } from "rxjs/webSocket";
-import { retry } from "rxjs/operators";
 
+/**
+ * Handles the sending and receiving of messages.
+ */
 export interface WsConnectionInterface {
   events(): Observable<WsEventBasic>;
   send<K extends keyof WsCommandMap>(type: K, data: WsCommandMap[K]): void
 }
 
-export class WsConnection implements WsConnectionInterface {
+export interface WsConnectionStatus {
+  status: "closed" | "connecting" | "connected" | "closing";
+  origin: "client" | "server";
+}
+
+/**
+ * Handles the connection control.
+ */
+export interface WsConnectionControlInterface {
+  open(): void;
+  close(): void;
+  status(): Observable<WsConnectionStatus>;
+}
+
+export interface WsConnectionConfig {
+  url: string;
+}
+
+export class WsConnection implements WsConnectionInterface, WsConnectionControlInterface {
+  private socket: WebSocket;
+  private readonly _events = new Subject<WsEventBasic>();
+  private readonly _status = new BehaviorSubject<WsConnectionStatus>({
+    origin: "client",
+    status: "closed"
+  });
+
   constructor(
+    private readonly config: WsConnectionConfig
   ) { }
 
   events(): Observable<WsEventBasic> {
-    throw new Error("Method not implemented.");
-  }
-  send<K extends keyof WsCommandMap>(type: K, data: WsCommandMap[K]): void {
-    throw new Error("Method not implemented.");
+    return this._events.asObservable();
   }
 
+  send<K extends keyof WsCommandMap>(type: K, data: WsCommandMap[K]): void {
+    if (this.socket === undefined) {
+      throw new Error("connection is closed, call `open` first");
+    }
+
+    const rawCommand = { command: type, data: data };
+    const rawCommandJson = JSON.stringify(rawCommand);
+    this.socket.send(rawCommandJson);
+  }
+
+  open(): void {
+    if (this.socket) {
+      throw new Error("connection is already created");
+    }
+
+    this.socket = new WebSocket(this.config.url);
+    this.socket.addEventListener("open", (ev) => {
+      this._status.next({ status: "connected", origin: "client" });
+    }, { once: true });
+
+    this.socket.addEventListener("message", (ev) => {
+      const event = JSON.parse(ev.data);
+      this._events.next(event);
+    });
+
+    this.socket.addEventListener("close", (ev) => {
+      this._status.next({
+        status: "closed",
+        origin: ev.reason === "client closes" ? "client" : "server"
+      });
+    });
+
+    this._status.next({ status: "connecting", origin: "client" });
+  }
+
+  close(): void {
+    if (this.socket === undefined) {
+      throw new Error("connection is closed, call `open` first");
+    }
+
+    this.socket.close(1000, "client closes");
+    this._status.next({ status: "closing", origin: "client" });
+    this.socket = undefined;
+  }
+
+  status(): Observable<WsConnectionStatus> {
+    return this._status.asObservable();
+  }
 }
