5966514e830d5a363a92b1db8305fd4b67ee3fc6
Wouter Lindenhof <wouterlindenhof@gmail.com>
2018-11-21 18:04:55 +0100

    Add menu-bar-component

--- web/src/app/gui/menu/menu.module.ts
+++ web/src/app/gui/menu/menu.module.ts
@@ -5,24 +5,30 @@ import { MenuComponent } from "./menu.component";
 import { MenuItemComponent } from "./menu-item/menu-item.component";
 import { MenuSpacerComponent } from "./menu-spacer/menu-spacer.component";
 import { MenuItemCheckboxComponent } from "./menu-item-checkbox/menu-item-checkbox.component";
+import { MenuBarComponent } from "./menu-bar/menu-bar.component";
 
+const publicComponents = [
+  MenuComponent,
+  MenuItemComponent,
+  MenuSpacerComponent,
+  MenuBarComponent,
+  MenuItemCheckboxComponent,
+];
+
+const privateComponents = [
+];
 
 @NgModule({
   declarations: [
-    MenuComponent,
-    MenuItemComponent,
-    MenuSpacerComponent,
-    MenuItemCheckboxComponent,
+    ...publicComponents,
+    ...privateComponents,
   ],
   imports: [
     CommonModule,
     FormsModule
   ],
   exports: [
-    MenuComponent,
-    MenuItemComponent,
-    MenuSpacerComponent,
-    MenuItemCheckboxComponent
+    ...publicComponents,
   ],
   providers: [],
 })
