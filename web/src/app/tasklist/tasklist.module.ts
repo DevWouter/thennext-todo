d4daa15c39ceab2514c386d617eada9a252e92bf
Wouter Lindenhof <wouterlindenhof@gmail.com>
2019-01-04 21:51:47 +0100

    Delete tasklist-settings-keyinput-panel

--- web/src/app/tasklist/tasklist.module.ts
+++ web/src/app/tasklist/tasklist.module.ts
@@ -9,7 +9,6 @@ import { TasklistComponent } from "./tasklist.component";
 import { TasklistItemComponent } from "./tasklist-item/tasklist-item.component";
 import { TasklistSettingsEncryptComponent } from "./tasklist-settings-encrypt/tasklist-settings-encrypt.component";
 import { TasklistSettingsEncryptPanelComponent } from "./tasklist-settings-encrypt-panel/tasklist-settings-encrypt-panel.component";
-import { TasklistSettingsKeyinputPanelComponent } from "./tasklist-settings-keyinput-panel/tasklist-settings-keyinput-panel.component";
 
 const publicComponents = [
   TasklistComponent,
@@ -19,7 +18,6 @@ const publicComponents = [
 const privateComponents = [
   TasklistItemComponent,
   TasklistSettingsEncryptPanelComponent,
-  TasklistSettingsKeyinputPanelComponent,
 ];
 
 @NgModule({
