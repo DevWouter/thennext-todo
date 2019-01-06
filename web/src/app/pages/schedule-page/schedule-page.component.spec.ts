a0a4d57665cf4832defb5732ba878e084c81b3e4
Wouter Lindenhof <wouterlindenhof@gmail.com>
2019-01-06 01:49:41 +0100

    Add test to switch back to tasks page

--- web/src/app/pages/schedule-page/schedule-page.component.spec.ts
+++ web/src/app/pages/schedule-page/schedule-page.component.spec.ts
@@ -1,24 +1,39 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject, } from '@angular/core/testing';
+import { RouterTestingModule, } from '@angular/router/testing';
 import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { DebugElement, Component } from '@angular/core';
+import { Location } from "@angular/common";
 
 import { SchedulePageComponent } from './schedule-page.component';
 import { MaterialModule } from '../../material.module';
+import { Router } from '@angular/router';
+
+@Component({
+  template: ''
+})
+class DummyComponent {
+}
 
 describe('SchedulePageComponent', () => {
   let component: SchedulePageComponent;
   let fixture: ComponentFixture<SchedulePageComponent>;
+  let location: Location;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [SchedulePageComponent],
-      imports: [MaterialModule]
+      declarations: [SchedulePageComponent, DummyComponent],
+      imports: [
+        RouterTestingModule.withRoutes(
+          [{ path: 'tasks', component: DummyComponent }]
+        ),
+        MaterialModule]
     })
       .compileComponents();
   }));
 
   beforeEach(() => {
+    location = TestBed.get(Location);
     fixture = TestBed.createComponent(SchedulePageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -27,4 +42,13 @@ describe('SchedulePageComponent', () => {
   it('should exist', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should switch back to task', async(inject([Router, Location], (router: Router, location: Location) => {
+    const viewListButton = fixture.debugElement.query(By.css("[data-cy='view-list']"));
+    viewListButton.triggerEventHandler("click", {});
+    fixture.whenStable().then(() => {
+      expect(location.path()).toBe("/tasks");
+    });
+  }))
+  );
 });
