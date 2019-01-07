7362c999a436dbfef02f6d7f13331e7e53ebc8be
Wouter Lindenhof <wouterlindenhof@gmail.com>
2019-01-07 19:03:47 +0100

    Move header of task-page into task-page.component

--- web/src/app/pages/task-page/task-page.component.ts
+++ web/src/app/pages/task-page/task-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { Observable } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
+
 import { ContextService, MediaViewService } from "../../services";
 import { MessageBusStateService } from "../../services/message-bus";
 
@@ -12,6 +15,7 @@ export class TaskPageComponent implements OnInit {
   size = 400;
   showPane = false;
   isMobile = false;
+  $taskListName: Observable<string>;
   constructor(
     private readonly contextService: ContextService,
     private readonly mediaViewService: MediaViewService,
@@ -25,9 +29,18 @@ export class TaskPageComponent implements OnInit {
         this.showPane = !!task;
       });
 
+    this.$taskListName = this.contextService.activeTaskList.pipe(
+      filter(x => !!x),
+      map(x => x.name)
+    );
+
     this.messageBusStateService.set("open");
 
     this.mediaViewService.extraSmall.subscribe(x => this.isMobile = x);
+  }
+
+  open() {
+
   }
 
   offsetPaneWidth(offset: number): void {
