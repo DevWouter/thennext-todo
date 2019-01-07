637ff1f6962ab7e3002d1bea03543c19d41acd9d
Wouter Lindenhof <wouterlindenhof@gmail.com>
2019-01-07 20:52:02 +0100

    Inline the menu of settings page component

--- web/src/app/pages/settings-page/settings-page.component.ts
+++ web/src/app/pages/settings-page/settings-page.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { MessageBusStateService } from "../../services/message-bus";
+import { Router } from '@angular/router';
+import { SessionService, NavigationService } from '../../services';
+import { MenuComponent } from '../../gui/menu';
 
 @Component({
   selector: "app-settings-page",
@@ -7,11 +10,44 @@ import { MessageBusStateService } from "../../services/message-bus";
   styleUrls: ["./settings-page.component.scss"]
 })
 export class SettingsPageComponent implements OnInit {
+  private _currentListUuid: string = undefined;
+  public get currentListUuid(): string { return this._currentListUuid; }
+  public set currentListUuid(v: string) { this._currentListUuid = v; this.updated(); }
+
+  @ViewChild(MenuComponent)
+  private menu: MenuComponent;
+
   constructor(
+    private readonly router: Router,
+    private readonly navigation: NavigationService,
+    private readonly sessionService: SessionService,
     private readonly messageBusStateService: MessageBusStateService,
   ) { }
 
   ngOnInit(): void {
     this.messageBusStateService.set("open");
-   }
+  }
+
+  close() {
+    this.menu.close();
+  }
+
+  open() {
+    this.menu.open();
+  }
+
+  goToTasks() {
+    this.router.navigate(["/tasks"]);
+  }
+
+  logout() {
+    if (confirm("Are you sure you want to logout?")) {
+      this.sessionService.logout();
+      this.router.navigate(["/"]);
+    }
+  }
+
+  updated() {
+    this.navigation.toTaskPage({ taskListUuid: this._currentListUuid, taskUuid: null });
+  }
 }
